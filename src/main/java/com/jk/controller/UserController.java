b159864fcadf212089e07eeb0df1dc4448ee6d07
zhangyd43 <1274975377@qq.com>
2020-08-19 15:13:55 +0800

    写了个乱码

--- src/main/java/com/jk/controller/UserController.java
+++ src/main/java/com/jk/controller/UserController.java
@@ -1,8 +1,13 @@
 package com.jk.controller;
 
 
-
+import javax.xml.ws.RequestWrapper;
 
 public class UserController {
     String a="huahua";
+
+    @RequestWrapper
+    public String add(){
+        return "hi who is you?";
+    }
 }
